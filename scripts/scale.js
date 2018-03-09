38cdf4fddcaba53b0a651ddcd0e0ddfddf19f2ea
Harry <harryboyd1@gmail.com>
2018-03-09 19:04:59 +1300

    1.4

--- scripts/scale.js
+++ scripts/scale.js
@@ -29,3 +29,19 @@ $(document).on("touchmove" , function (event) {
   $(".home.br").css("height", $(window).height() - event.pageY)
 
 })
+
+$(document).on("touchstart" , function (event) {
+
+  $(".home.tl").css("width", event.pageX)
+  $(".home.tl").css("height", event.pageY)
+
+  $(".home.tr").css("width", $(window).width() - event.pageX)
+  $(".home.tr").css("height", event.pageY)
+
+  $(".home.bl").css("width", event.pageX)
+  $(".home.bl").css("height", $(window).height() - event.pageY)
+
+  $(".home.br").css("width", $(window).width() - event.pageX)
+  $(".home.br").css("height", $(window).height() - event.pageY)
+
+})
