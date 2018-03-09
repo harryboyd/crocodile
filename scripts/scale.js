959d6405e38966c352df7cd45329e77e27a0eee2
Harry <harryboyd1@gmail.com>
2018-03-09 18:27:38 +1300

    1.1

--- scripts/scale.js
+++ scripts/scale.js
@@ -13,3 +13,19 @@ $(document).on("mousemove" , function (event) {
   $(".home.br").css("height", $(window).height() - event.pageY)
 
 })
+
+$(document).on("touchmove" , function (event) {
+
+  $(".home.tl").css("width", event.pageX)
+  $(".home.tl").css("height", event.pageY)
+
+  $(".home.tr").css("width", $(window).width() - event.pageX)
+  $(".home.tr").css("height", event.pageY)
+
+  $(".home.bl").css("width", event.pageX)
+  $(".home.bl").css("height", $(window).height() - event.pageY)
+
+  $(".home.br").css("width", $(window).width() - event.pageX)
+  $(".home.br").css("height", $(window).height() - event.pageY)
+
+})
